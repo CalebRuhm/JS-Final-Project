a5f973c8bc08149a29302c85d52e0e892576fa4e
Caleb Ruhm <ruhmc0619@gmail.com>
2022-01-11 21:27:45 -0500

    remove unwanted files

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 (function () {
+
+  const apiKey = "fada96de47ae8a516ca0917a1196a438";
+  let city = "Albany";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="
+   + city
+   + "&units=metric&appid="
+   + apiKey;
   
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Albany&units=metric&appid=fada96de47ae8a516ca0917a1196a438"
-  
-  axios.get(url).then(response => console.log(response));
+  axios.get(url)
+  .then(response => console.log(response));
+
+  let displayWeather = (response) => {
+
+  };
 
 })();
