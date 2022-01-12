12bd7991de35ab4500f0fc4b83332beb5008cbeb
Caleb Ruhm <ruhmc0619@gmail.com>
2022-01-11 21:15:40 -0500

    uninstall axios

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 (function () {
   
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Albany&units=metric&appid=fada96de47ae8a516ca0917a1196a438"
+  
+  axios.get(url).then(response => console.log(response));
 
 })();
