e7176b3fdce5039750a5aae7169943e9efe9be9e
Caleb Ruhm <ruhmc0619@gmail.com>
2022-01-12 00:21:15 -0500

    weather serach fix

--- script.js
+++ script.js
@@ -1,46 +1,50 @@
 (function () {
-  // Converts variable to usable API URL string
-  const apiKey = "fada96de47ae8a516ca0917a1196a438";
-  let city = "Kyoto";
-  const url = "https://api.openweathermap.org/data/2.5/weather?q="
-   + city
-   + "&units=metric&appid="
-   + apiKey;
 
-  // Axios to get API
-  axios({
-    method: "GET",
-    url: url,
-  })
-  // Push axios response data to html
-  .then((response) => {
-    const cityName = response.data.name;
-    const temp = response.data.main.temp;
-    const newTemp = Math.round((temp * 9 / 5) + 32);
-    const icon = response.data.weather.map(e => e.icon);
-    const detail = response.data.weather.map(e => e.description);
-    const humidity = response.data.main.humidity;
-    const wind = response.data.wind.speed;
-    let newWind = Math.round((wind / 1.609344) * 100) / 100;
-    document.querySelector(".city").textContent = cityName;
-    document.querySelector(".temperature").textContent = newTemp + "°" + "F";
-    document.querySelector(".weatherIcon").src = "https://openweathermap.org/img/wn/" + icon +"@2x.png";
-    document.querySelector(".info").textContent = detail;
-    document.querySelector(".humidity").textContent = `Humidity: ${humidity}%`;
-    document.querySelector(".wind").textContent = `${newWind} mp/h`;
-  });
-
-
-  // Search bar event listener calls search function
-  document.querySelector(".searchBtn").addEventListener("click", (e) => {
-    search();
-  })
+  const displayWeather = (city) => {
+    // Converts variable to usable API URL string
+    const apiKey = "fada96de47ae8a516ca0917a1196a438";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="
+     + city
+     + "&units=imperial&appid="
+     + apiKey;
   
-  // Search function to update city
-  const search = (city) => {
-    let searchVale = document.querySelector(".searchBar").value;
-    city = searchVale;
-    console.log(city);
+    // Axios to get API
+    axios({
+      method: "GET",
+      url: url,
+    })
+    // Push axios response data to html
+    .then((response) => {
+      const cityName = response.data.name;
+      const temp = response.data.main.temp;
+      const newTemp = Math.round((temp));
+      const icon = response.data.weather.map(e => e.icon);
+      const detail = response.data.weather.map(e => e.description);
+      const humidity = response.data.main.humidity;
+      const wind = response.data.wind.speed;
+      let newWind = Math.round(wind);
+      document.querySelector(".city").textContent = cityName;
+      document.querySelector(".temperature").textContent =newTemp + "°" + "F";
+      document.querySelector(".weatherIcon").src = "https://openweathermap.org/img/wn/" + icon +"@2x.png";
+      document.querySelector(".info").textContent = detail;
+      document.querySelector(".humidity").textContent = `Humidity: ${humidity}%`;
+      document.querySelector(".wind").textContent = `${newWind} mp/h`;
+    });
   }
+
+  // Search button listener to update city
+  const search = () => {
+    document.querySelector(".searchBtn").addEventListener("click", () => {
+      let city = document.querySelector(".searchBar").value;
+      displayWeather(city);
+      document.querySelector(".weatherIcon").classList.remove("hidden");
+      document.querySelector(".searchBar").value = "";
+    })
+  }
+  
+  search();
+
+  // Initial Page State
+  document.querySelector(".weatherIcon").classList.add("hidden");
 
 })();
